5ba980af5352f6c9dde8a288d376aa2642eea30e
David <David@DESKTOP-VJMTNVA>
2018-07-23 21:15:53 +0100

    fibonacci series

--- SumOfFibonnaci/Program.cs
+++ SumOfFibonnaci/Program.cs
@@ -14,13 +14,17 @@ namespace SumOfFibonnaci
             FibonacciSeries();
         }
 
+        //this method helps calculate the fibonacci series
         public static void FibonacciSeries()
         {
             Console.WriteLine("Enter a number to print the fibonacci series");
             var num = Convert.ToInt32(Console.ReadLine());
+            //initialize default values
             int sum = 1;
-            int start = 0;
+            int start = 0; 
             int next = 1;
+
+            //loop is used to stop the program at exact number the user enters
             for(int i = 1; i <= num; i++)
             {
                 sum = start + next;
